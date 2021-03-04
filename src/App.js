4cdc718906d693d78eaf2902ec8f22096e0ff0a3
adyang94 <adyang94@yahoo.com>
2021-03-03 19:16:47 -0600

    updates to general info

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import GeneralInfo from './components/generalInfo';
 
 function App() {
   return (
     <div className="App">
-      
+      <GeneralInfo />
     </div>
   );
 }
--- src/components/generalInfo.js
+++ src/components/generalInfo.js
@@ -1,2 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {useState} from 'react';
+
+function GeneralInfo (props) {
+
+  const [generalInfoData, setGeneralInfo] = useState({
+    name: '',
+    email: '',
+    phoneNumber: ''
+  });
+
+  const saveGeneralInfo = (newInfo) => {
+    setGeneralInfo(
+      ...generalInfoData,
+
+      )
+  };
+
+  return (  //JSX
+    <div>
+      <h1>General Info:</h1><br></br>
+      <h4>Name</h4>
+        <input
+          name = 'name'
+          type = 'text'
+          value = {generalInfoData.name}
+          onChange = {saveGeneralInfo}
+          id = 'generalInfoNameInput'
+          placeholder = 'Insert Name'
+        />
+      <h4>Email</h4>
+        <input
+          name = 'email'
+          type = 'email'
+          value = {generalInfoData.email}
+          onChange = {saveGeneralInfo}
+          id = 'generalInfoEmailInput'
+          placeholder = 'Insert Email'
+        />
+      <h4>Phone Number</h4>
+        <input
+          name = 'phoneNumber'
+          type = 'number'
+          value = {generalInfoData.phoneNumber}
+          onChange = {saveGeneralInfo}
+          id = 'generalInfoPhoneNumber'
+          placeholder = 'Add Phone Number'
+        />
+
+    </div>
+  )
+};
+export default GeneralInfo;
