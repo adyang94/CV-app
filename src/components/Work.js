077cfc0d0f065510e9aea22c04844f629b37f87c
adyang94 <adyang94@yahoo.com>
2021-03-05 11:49:55 -0600

    work section rendering done and adjusting CSS

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,19 +6,66 @@ function WorkSection () {
   
   const addWorkExperience = () => {
     console.log('hi');
-    RenderForm();
-    return(
-      <div>
-        <h6>Add Work Experience:</h6>
-      </div>
-    )
+        //select HTML element
+    const formsCtn = document.querySelector('.formsCtn-WE');
+
+    const formCtn = document.createElement('div');
+      formCtn.classList.add('formCtn');
+      
+    const companyInputCtn = document.createElement('div');
+      companyInputCtn.innerHTML = 'Company:';
+      companyInputCtn.classList.add('companyInputCtn');
+    const companyInput = document.createElement('input');
+      companyInput.classList.add('companyInput');
+
+    const positionInputCtn = document.createElement('div');
+      positionInputCtn.innerHTML = 'Position Title:';
+      positionInputCtn.classList.add('positionInputCtn');
+    const positionInput = document.createElement('input');
+      positionInput.classList.add('positionInput');
+
+    const responsibilitiesInputCtn = document.createElement('div');
+      responsibilitiesInputCtn.innerHTML = 'Responsibilities:';
+      responsibilitiesInputCtn.classList.add('responsibilitiesInputCtn');
+    const responsibilitiesInput = document.createElement('input');
+      responsibilitiesInput.classList.add('responsibilitiesInput');
+
+    const startDateInputCtn = document.createElement('div');
+      startDateInputCtn.classList.add('startDateInputCtn');
+      startDateInputCtn.innerHTML = 'Start Date:'
+    const startDateInput = document.createElement('input');
+      startDateInput.classList.add('startDateInput');
+
+    const endDateInputCtn = document.createElement('div');
+      endDateInputCtn.classList.add('endDateInputCtn');
+      endDateInputCtn.innerHTML = 'End Date:'
+    const endDateInput = document.createElement('input');
+      endDateInput.classList.add('endDateInput');
+
+
+    //append to forms container.
+    companyInputCtn.appendChild(companyInput);
+    formCtn.appendChild(companyInputCtn);
+
+    positionInputCtn.appendChild(positionInput);
+    formCtn.appendChild(positionInputCtn);
+    
+    responsibilitiesInputCtn.appendChild(responsibilitiesInput);
+    formCtn.appendChild(responsibilitiesInputCtn);
+
+    startDateInputCtn.appendChild(startDateInput);
+    formCtn.appendChild(startDateInputCtn);
+
+    endDateInputCtn.appendChild(endDateInput);
+    formCtn.appendChild(endDateInputCtn);
+
+    formsCtn.appendChild(formCtn);
   }
 
   return (
     <div className = 'workExperience'>
+      <h3>Work Experience</h3>
       <div className = 'formsCtn-WE'></div>
-
-      <RenderForm/>
       <AddBtn 
         title = 'Work Experience' 
         id = 'addWorkExperienceBtn'
@@ -31,33 +78,3 @@ function WorkSection () {
 
 export default WorkSection;
 
-    // //select HTML element
-    // const formsCtn = document.querySelector('.formsCtn-WE');
-
-    // const formCtn = document.createElement('div');
-    //   formCtn.classList.add('formCtn');
-    
-    // const companyInputLabel = document.createElement
-    //   const companyInput = document.createElement('input');
-    //   companyInput.classList.add('companyInput');
-
-    // const positionInput = document.createElement('input');
-    //   positionInput.classList.add('positionInput');
-
-    // const responsibilitiesInput = document.createElement('input');
-    //   responsibilitiesInput.classList.add('responsibilitiesInput');
-
-    // const startDateInput = document.createElement('input');
-    //   startDateInput.classList.add('startDateInput');
-
-    // const endDateInput = document.createElement('input');
-    //   endDateInput.classList.add('endDateInput');
-
-
-    // //append to forms container.
-    // formCtn.appendChild(companyInput);
-    // formCtn.appendChild(positionInput);
-    // formCtn.appendChild(responsibilitiesInput);
-    // formCtn.appendChild(startDateInput);
-    // formCtn.appendChild(endDateInput);
-    // formsCtn.appendChild(formCtn);
